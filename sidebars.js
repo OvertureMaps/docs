38be1bee101a61860735e879a974cab59d25c06c
Dana Bauer <dana.bauer@gmail.com>
2024-02-12 13:24:08 -0800

    finalize sidebar

--- sidebars.js
+++ sidebars.js
@@ -2,33 +2,43 @@
 const sidebars = {
   docs: [
     'index',
+
     {
       type: 'category',
       label: 'Accessing Data',
+      link: {
+        type: 'doc',
+        id: 'accessing-data/index'
+      },
       collapsed: false,
       items: [
         'accessing-data/cloud-services',
-        'accessing-data/locally',
-        'accessing-data/index'
+        'accessing-data/locally'
       ]
     },
     {
       type: 'category',
       label: 'Exploring Data',
+      link: {
+        type: 'doc',
+        id: 'exploring-data/index'
+      },
       collapsed: false,
       items: [
         'exploring-data/build-a-map',
-        'exploring-data/community-projects',
-        'exploring-data/index'
+        'exploring-data/community-projects'
       ]
     },
     {
       type: 'category',
       label: 'Visualizing Data',
+      link: {
+        type: 'doc',
+        id: 'visualizing-data/index'
+      },
       collapsed: false,
       items: [
-        'visualizing-data/visualizing-places',
-        'visualizing-data/index'
+        'visualizing-data/visualizing-places'
       ]
     }
   ]
