2c7fcc1010052e01350aca5c671bf6e5d6404cd9
Dana Bauer <dana.bauer@gmail.com>
2024-03-12 14:56:29 -0400

    fix country level query

---
+++ src/queries/duckdb/country_level_geometries.sql
@@ -0,0 +1,30 @@
+LOAD httpfs;
+LOAD spatial;
+SET s3_region='us-west-2';
+
+CREATE VIEW admins_view AS (
+    SELECT
+        *
+    FROM
+        read_parquet('s3://overturemaps-us-west-2/release/2024-02-15-alpha.0/theme=admins/type=*/*', filename=true, hive_partitioning=1)
+);
+COPY (
+    SELECT
+            admins.id,
+            admins.subType,
+            admins.isoCountryCodeAlpha2,
+            names.primary AS primary_name,
+            sources[1].dataset AS primary_source,
+            areas.areaId,
+            ST_GeomFromWKB(areas.areaGeometry) as geometry
+    FROM admins_view AS admins
+    INNER JOIN (
+        SELECT
+            id as areaId,
+            localityId,
+            geometry AS areaGeometry
+        FROM admins_view
+    ) AS areas ON areas.localityId == admins.id
+    WHERE admins.adminLevel = 1
+) TO 'countries.geojson'
+WITH (FORMAT GDAL, DRIVER 'GeoJSON');
