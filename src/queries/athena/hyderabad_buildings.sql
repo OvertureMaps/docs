8939c64ead55a57484d56e22bddaa2f43bb0ab17
Dana Bauer <dana.bauer@gmail.com>
2024-04-18 11:59:44 -0400

    update queries

--- src/queries/athena/hyderabad_buildings.sql
+++ src/queries/athena/hyderabad_buildings.sql
@@ -2,14 +2,13 @@ SELECT
     id,
     theme,
     class,
-    CAST(sources AS JSON) AS sources,
     sources[1].dataset AS primary_source,
-    CAST(names AS JSON) AS names,
+    names.primary AS primary_name,
     ST_GeomFromBinary(geometry) AS geometry
 FROM overture.release.__ATHENA_OVERTURE_RELEASE
 WHERE theme='buildings'
     AND type='building'
-    AND  bbox.xmin > 78.4194
-    AND bbox.xmax < 78.5129
-    AND bbox.ymin > 17.3427
-    AND bbox.ymax < 17.4192
+    AND  bbox.xmin > 78.383144
+    AND bbox.xmax < 78.565011
+    AND bbox.ymin > 17.30199
+    AND bbox.ymax < 17.423426
