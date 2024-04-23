38931e1bba65a41411d111e0af73bc2dbd053b69
Jennings Anderson <jennings.anderson@gmail.com>
2024-04-23 13:07:03 -0700

    Pull queries out into their own sql files for better testing integration

---
+++ src/queries/duckdb/seattle_placenames_for_map.sql
@@ -0,0 +1,16 @@
+LOAD spatial;
+LOAD azure;
+-- Access the data on Microsoft Azure in this example
+SET azure_storage_connection_string = 'DefaultEndpointsProtocol=https;AccountName=overturemapswestus2;AccountKey=;EndpointSuffix=core.windows.net';
+COPY (
+    SELECT
+        subtype,
+        locality_type,
+        names.primary as name,
+        ST_GeomFromWKB(geometry) as geometry
+    FROM read_parquet('azure://release/2024-04-16-beta.0/theme=admins/type=locality/*',
+                        filename=true, hive_partitioning=1)
+    WHERE bbox.xmin > -122.679404 AND bbox.xmax < -121.978275
+    AND bbox.ymin > 47.360619 AND bbox.ymax < 47.786336
+) TO 'seattle_placenames.geojson'
+WITH (FORMAT GDAL, DRIVER 'GeoJSON', SRS 'EPSG:4326');
---
+++ src/queries/duckdb/seattle_roads_for_map.sql
@@ -0,0 +1,15 @@
+LOAD spatial;
+LOAD httpfs;
+COPY (
+    SELECT
+        level,
+        names.primary AS name,
+        JSON_EXTRACT_STRING(road, '$.class') AS class,
+        ST_GeomFromWKB(geometry) as geometry
+    FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=transportation/type=segment/*')
+    WHERE
+        subtype = 'road'
+        AND bbox.xmin > -122.68 AND bbox.xmax < -121.98
+        AND bbox.ymin > 47.36 AND bbox.ymax < 47.79
+) TO 'seattle_roads.geojsonseq'
+WITH (FORMAT GDAL, DRIVER 'GeoJSONSeq', SRS 'EPSG:4326');
