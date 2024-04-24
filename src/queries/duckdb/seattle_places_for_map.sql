769d7e6118bc86c38c515acbd6f2ce33cc056a40
Jennings Anderson <jenningsa@meta.com>
2024-04-24 08:11:29 -0700

    Pull queries out of example pages (#32)
    * Pull queries out into their own sql files for better testing integration
    
    * pulling out qgis examples

---
+++ src/queries/duckdb/seattle_places_for_map.sql
@@ -0,0 +1,17 @@
+LOAD spatial;
+LOAD httpfs;
+-- Access the data on AWS in this example
+SET s3_region='us-west-2';
+
+COPY (
+    SELECT
+        names.primary AS name,
+        categories.main as category,
+        ROUND(confidence,2) as confidence,
+        ST_GeomFromWKB(geometry) as geometry
+FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELASE/theme=places/*/*')
+WHERE
+    -- Point geometry doesn't require looking at both min and max:
+    bbox.xmin BETWEEN -122.68 AND -121.98 AND
+    bbox.ymin BETWEEN 47.36 AND 47.79
+) TO 'seattle_places.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON', SRS 'EPSG:4326');
---
+++ src/queries/duckdb/seattle_water_for_map.sql
@@ -0,0 +1,14 @@
+LOAD spatial;
+LOAD httpfs;
+COPY (
+    SELECT
+        subtype,
+        class,
+        names.primary AS name,
+        ST_GeomFromWKB(geometry) as geometry
+    FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=base/type=water/*')
+    WHERE
+        subtype in ('ocean', 'lake', 'pond', 'reservoir', 'river', 'stream', 'water', 'canal')
+        AND bbox.xmin > -123 AND bbox.xmax < -122
+        AND bbox.ymin > 47 AND bbox.ymax < 48
+) TO 'seattle_water.geojsonseq' WITH (FORMAT GDAL, DRIVER 'GeoJSONSeq', SRS 'EPSG:4326');
