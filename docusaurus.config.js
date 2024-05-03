a47ca6cb1c0201a57e85033d60f30191c4a13c2d
Dana Bauer <dana.bauer@gmail.com>
2024-05-03 16:57:15 -0400

    search first steps

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,6 +126,32 @@ const config = {
           }
         ],
       },
+      algolia: {
+        appId: 'MK8X1051PQ',
+        //this is the public search API key; ok to commit  
+        apiKey:'d0841a742af1d636744261d21d4142bb',
+        indexName: 'overturemaps',
+        contextualSearch: true, 
+
+        /*other optional Algolia params
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
+        */
+      },
       footer: {
         style: 'dark',
         copyright: `Copyright © ${new Date().getFullYear()} Overture Maps Foundation.`,
