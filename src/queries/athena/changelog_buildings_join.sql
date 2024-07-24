ab0e65dd3c6539dc841a6933b43c4a7717e48745
Dana Bauer <dana.bauer@gmail.com>
2024-07-23 22:00:51 -0400

    add GERS changelog queries

---
+++ src/queries/athena/changelog_buildings_join.sql
@@ -0,0 +1,10 @@
+SELECT release.id, release.names.primary AS primary_name, release.sources[1].dataset AS primary_source, change_type, ST_GeomFromBinary(geometry) AS geometry
+FROM v2024_06_13_beta_0 AS release
+        INNER JOIN changelog_v2024_06_13_beta_0 AS changelog ON release.id = changelog.id
+WHERE release.theme = 'buildings'
+ AND release.bbox.xmin > 37.165914
+        AND release.bbox.xmax < 37.902271
+        AND release.bbox.ymin > 10.221917
+        AND release.bbox.ymax < 10.751245;
+
+
