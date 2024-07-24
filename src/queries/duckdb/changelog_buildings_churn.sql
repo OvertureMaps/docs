bdfdd136392688a6f5b491c78035d9215ab64d83
Dana Bauer <dana.bauer@gmail.com>
2024-07-24 17:09:26 -0400

    improvements to GERS documentation (#67)
    * add GERS changelog queries
    
    * edits and formatting
    
    * rewrite demonstration page and add graphic
    
    * formatting

---
+++ src/queries/duckdb/changelog_buildings_churn.sql
@@ -0,0 +1,12 @@
+LOAD spatial;
+LOAD httpfs;
+SET s3_region='us-west-2';
+
+SELECT id, type, change_type 
+FROM read_parquet('s3://overturemaps-us-west-2/changelog/2024-06-13-beta.0/theme=buildings/type=*/change_type=*/*', filename=true, hive_partitioning=1) 
+WHERE 
+        bbox.xmin > 37.165914
+        AND bbox.xmax < 37.902271
+        AND bbox.ymin > 10.221917
+        AND bbox.ymax < 10.751245;
+
