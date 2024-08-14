3a4a3beb9b4c25fec6565c848c34a586c2678fe3
jeffdefacto <jeffdefacto@gmail.com>
2024-08-14 13:12:03 -0700

    minor formatting and typos

--- src/queries/athena/transportation_speed_limits.sql
+++ src/queries/athena/transportation_speed_limits.sql
@@ -1,9 +1,9 @@
 SELECT id,
     speed_limits,
-    ST_GeomFromBinary(geometry) as geometry
+    ST_GeomFromBinary(geometry) AS geometry
 FROM __ATHENA_OVERTURE_RELEASE
 WHERE type = 'segment'
-	AND any_match(
+	AND ANY_MATCH(
 		speed_limits,
 		speed_limit -> speed_limit.max_speed.value = 27
         AND speed_limit.max_speed.unit = 'mph'
