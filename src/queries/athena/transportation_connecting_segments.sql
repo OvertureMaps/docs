17afc156700b3f0c81cd75e84a1ac547de59fd5d
jeffdefacto <jeffdefacto@gmail.com>
2024-08-14 17:08:08 -0700

    Transportation guide updates (#86)
    * First pass at transportation guide
    
    This contains updates related to the transportation guide including queries and some visuals.
    
    * added some additional property descriptions
    
    added some additional property descriptions
    
    * replaced graphic with svg
    
    updated concept visual with svg
    
    * Update docs/guides/transportation.mdx
    
    Co-authored-by: Brad Richardson <brad-richardson@users.noreply.github.com>
    
    * Update docs/guides/transportation.mdx
    
    Co-authored-by: Brad Richardson <brad-richardson@users.noreply.github.com>
    
    * Fixed some issues
    
    Corrected issues per Brad's comments
    
    * Updated class counts for august
    
    * made counts human readable
    
    * minor formatting fix
    
    * minor formatting and typos
    
    ---------
    
    Co-authored-by: Brad Richardson <brad-richardson@users.noreply.github.com>

---
+++ src/queries/athena/transportation_connecting_segments.sql
@@ -0,0 +1,15 @@
+WITH input AS (
+    SELECT id AS input_id,
+        connector_id
+    FROM __ATHENA_OVERTURE_RELEASE
+        CROSS JOIN UNNEST(connector_ids) AS t(connector_id)
+    WHERE type = 'segment'
+        AND id = '08628d5437ffffff0473ffc36df547db'
+)
+SELECT *,
+    ST_GEOMFROMBINARY(geometry) AS geometry
+FROM __ATHENA_OVERTURE_RELEASE,
+    input
+WHERE type = 'segment'
+    AND id != input_id
+    AND CONTAINS(connector_ids, input.connector_id)
