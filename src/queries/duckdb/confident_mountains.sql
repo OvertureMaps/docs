6c2eb79d71dbea50d2a8cc240a04f4dc9510c1f2
Stephen Epps <stephenepps@fastmail.com>
2024-09-03 18:18:11 -0700

    update field in mountains sql

--- src/queries/duckdb/confident_mountains.sql
+++ src/queries/duckdb/confident_mountains.sql
@@ -13,7 +13,7 @@ COPY(
        sources[1].dataset AS primary_source,
        confidence
     FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=places/type=*/*', filename=true, hive_partitioning=1)
-    WHERE main_category = 'mountain' AND confidence > .90
+    WHERE primary_category = 'mountain' AND confidence > .90
     ORDER BY confidence DESC
 ) TO 'overture_places_mountains_gt90.geojson'
 WITH (FORMAT GDAL, DRIVER 'GeoJSON');
