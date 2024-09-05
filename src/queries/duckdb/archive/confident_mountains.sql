7c38ee6588e3cbb9c44fb8e62e960b2a2d5d0a91
Jennings Anderson <jenningsa@meta.com>
2024-09-05 09:23:30 -0700

    Various DuckDB updates (#94)
    * updated duckdb page
    
    * update
    
    * cleanup
    
    * renames
    
    * better examples
    
    * fix broken build

--- src/queries/duckdb/archive/confident_mountains.sql
+++ src/queries/duckdb/archive/confident_mountains.sql
@@ -6,14 +6,12 @@ COPY(
     SELECT
        id,
        names.primary as primary_name,
-       bbox.xmin as x,
-       bbox.ymin as y,
        ST_GeomFromWKB(geometry) as geometry,
        categories.primary as primary_category,
        sources[1].dataset AS primary_source,
        confidence
     FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=places/type=*/*', filename=true, hive_partitioning=1)
-    WHERE main_category = 'mountain' AND confidence > .90
+    WHERE categories.primary = 'mountain' AND confidence > .90
     ORDER BY confidence DESC
 ) TO 'overture_places_mountains_gt90.geojson'
 WITH (FORMAT GDAL, DRIVER 'GeoJSON');
