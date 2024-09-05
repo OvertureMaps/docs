890ad378b6efe81f78d75c0c20d818ed623748f6
Jennings Anderson <jennings.anderson@gmail.com>
2024-09-04 18:17:42 -0700

    better examples

--- src/queries/duckdb/new_york_pizza.sql
+++ src/queries/duckdb/new_york_pizza.sql
@@ -2,17 +2,16 @@ LOAD spatial;
 LOAD httpfs;
 SET s3_region='us-west-2';
 
-COPY(
+COPY(                                       -- COPY <query> TO <output> saves the results to disk.
     SELECT
        id,
        names.primary as name,
        confidence AS confidence,
-       'https://facebook.com/' || sources[1].record_id AS facebook_page,
-       CAST(socials AS JSON) as socials,
-       ST_GeomFromWKB(geometry) as geometry
+       CAST(socials AS JSON) as socials,    -- Ensure each attribute can be serialized to JSON
+       ST_GeomFromWKB(geometry) as geometry -- GDAL understands this to be the geometry
     FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=places/type=place/*', filename=true, hive_partitioning=1)
     WHERE categories.primary = 'pizza_restaurant'
-    AND bbox.xmin BETWEEN -75 AND -73
-    AND bbox.ymin BETWEEN 40 AND 41
-) TO 'nyc_pizza.geojson'
-WITH (FORMAT GDAL, DRIVER 'GeoJSON');
+    AND bbox.xmin BETWEEN -75 AND -73       -- Only use the bbox min values
+    AND bbox.ymin BETWEEN 40 AND 41         -- because they are point geometries.
+
+) TO 'nyc_pizza.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
