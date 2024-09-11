3c1f3aa685d6e2b5b9e704cb776d1d41af143961
Jennings Anderson <jennings.anderson@gmail.com>
2024-09-11 03:18:12 -0500

    fix linting errors

--- src/queries/duckdb/boulder_co_duckdb.sql
+++ src/queries/duckdb/boulder_co_duckdb.sql
@@ -1,8 +1,9 @@
-COPY(
-    SELECT
-        *
-    FROM
-        read_parquet("s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=*/type=*/*", union_by_name=True)
+COPY (
+    SELECT *
+    FROM read_parquet("s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=*/type=*/*", union_by_name = True)
     WHERE
-        bbox.xmin >= -105.30 AND bbox.ymin >= 39.98 AND bbox.xmax <= -105.24 AND bbox.ymax <= 40.07
+        bbox.xmin >= -105.30
+        AND bbox.ymin >= 39.98
+        AND bbox.xmax <= -105.24
+        AND bbox.ymax <= 40.07
 ) TO 'boulder_co_overture.parquet';
