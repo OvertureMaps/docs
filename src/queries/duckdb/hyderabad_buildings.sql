20ed10b3347e001cba193bd99328cf3e9402b8ba
Jennings Anderson <jenningsa@meta.com>
2024-09-17 06:30:36 -0700

    Update hyderabad_buildings.sql (#102)

--- src/queries/duckdb/hyderabad_buildings.sql
+++ src/queries/duckdb/hyderabad_buildings.sql
@@ -1,5 +1,4 @@
 LOAD spatial; -- noqa
-LOAD httpfs;  -- noqa
 
 COPY (
     SELECT
@@ -8,12 +7,11 @@ COPY (
         height,
         names.primary AS primary_name,
         sources[1].dataset AS primary_source,
-        ST_GeomFromWkb(geometry) AS geometry
-    FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=buildings/type=*/*', filename=true, hive_partitioning=1)
+        geometry
+    FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=buildings/type=*/*', hive_partitioning=1)
     WHERE
         bbox.xmin > 78.4194
         AND bbox.xmax < 78.5129
         AND bbox.ymin > 17.3427
         AND bbox.ymax < 17.4192
-) TO 'buildings_hyderabad.geojson'
-WITH (FORMAT GDAL, DRIVER 'GeoJSON');
+) TO 'buildings_hyderabad.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
