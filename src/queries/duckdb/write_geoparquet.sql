85e5e00d6ef7e3f647f86423761a6b5eb229d812
Daniel Sparing <daniel@sparing.nl>
2025-04-16 19:34:43 +0200

    move query to separate sql (like the other tabs)

---
+++ src/queries/duckdb/write_geoparquet.sql
@@ -0,0 +1,6 @@
+COPY(
+  SELECT
+    *
+  FROM read_parquet('s3://overturemaps-us-west-2/release/2024-08-20.0/theme=places/type=place/*', filename=true, hive_partitioning=1)
+  LIMIT 100000
+) TO 'places.parquet';
