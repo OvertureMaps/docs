41e036fd4f89caed59b2a36dbdddf57daf00066b
Jennings Anderson <jenningsa@meta.com>
2025-04-16 11:53:39 -0700

    Update write_geoparquet.sql

--- src/queries/duckdb/write_geoparquet.sql
+++ src/queries/duckdb/write_geoparquet.sql
@@ -1,9 +1,8 @@
 LOAD spatial; -- noqa
-SET s3_region='us-west-2';
 
 COPY(
   SELECT
     *
-  FROM read_parquet('s3://overturemaps-us-west-2/release/2024-08-20.0/theme=places/type=place/*', filename=true, hive_partitioning=1)
+  FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=places/type=place/*')
   LIMIT 100000
 ) TO 'places.parquet';
