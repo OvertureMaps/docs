b3ddeb74e943190fbd35b678d2a615d87f8ec046
Jennings Anderson <jenningsa@meta.com>
2025-05-09 12:34:17 -0700

    Cleaning up the Azure Queries (#183)

--- src/queries/duckdb/seattle_placenames_for_map.sql
+++ src/queries/duckdb/seattle_placenames_for_map.sql
@@ -1,15 +1,12 @@
 LOAD spatial; -- noqa
-LOAD azure;  -- noqa
 
--- Access the data on Microsoft Azure in this example
-SET azure_storage_connection_string = 'DefaultEndpointsProtocol=https;AccountName=overturemapswestus2;AccountKey=;EndpointSuffix=core.windows.net';
 COPY (
     SELECT
         subtype,
         locality_type,
         names.primary as name,
         geometry -- DuckDB v.1.1.0 will autoload this as a `geometry` type
-    FROM read_parquet('azure://release/2024-04-16-beta.0/theme=admins/type=locality/*', hive_partitioning=1)
+    FROM read_parquet('az://overturemapswestus2.blob.core.windows.net/release/2024-04-16-beta.0/theme=admins/type=locality/*', hive_partitioning=1)
     WHERE bbox.xmin > -122.679404 AND bbox.xmax < -121.978275
     AND bbox.ymin > 47.360619 AND bbox.ymax < 47.786336
 ) TO 'seattle_placenames.geojson'
