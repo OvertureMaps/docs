95aee0d9ec221f142767f39c4d1a93f356ce74d8
Stephen Epps <stephen@overturemaps.org>
2025-06-18 08:27:45 -0700

    Update duckdb query examples (#195)
    * update duckdb query examples, adjust transportation example with note
    
    * adjust duckdb queries
    
    * update paris transportation duckdb example, add queries
    
    * update paris road queries

--- src/queries/duckdb/mountain_peaks.sql
+++ src/queries/duckdb/mountain_peaks.sql
@@ -3,14 +3,17 @@ LOAD spatial; -- noqa
 SET s3_region='us-west-2';
 
 COPY(
-    SELECT
-       id,
-       names.primary as name,
-       CAST(elevation * 3.28084 AS INT) AS elevation_ft,
-       geometry -- DuckDB v.1.1.0 will autoload this as a `geometry` type
-    FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=base/type=land/*', filename=true, hive_partitioning=1)
-    WHERE subtype = 'physical' AND class IN ('peak','volcano') AND elevation IS NOT NULL
+  SELECT
+    id,
+    names.primary as name,
+    CAST(elevation * 3.28084 AS INT) AS elevation_ft,
+    geometry -- DuckDB v.1.1.0 will autoload this as a `geometry` type
+  FROM
+    read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=base/type=land/*', filename=true, hive_partitioning=1)
+  WHERE
+    subtype = 'physical'
+    AND class IN ('peak','volcano')
+    AND elevation IS NOT NULL
     AND bbox.xmin BETWEEN -124.71 AND -116.47
     AND bbox.ymin BETWEEN 41.99 AND 46.30
-) TO 'oregon_peaks.geojson'
-WITH (FORMAT GDAL, DRIVER 'GeoJSON');
+) TO 'oregon_peaks.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
--- src/queries/duckdb/new_york_pizza.sql
+++ src/queries/duckdb/new_york_pizza.sql
@@ -3,15 +3,16 @@ LOAD spatial; -- noqa
 SET s3_region='us-west-2';
 
 COPY(                                       -- COPY <query> TO <output> saves the results to disk.
-    SELECT
-       id,
-       names.primary as name,
-       confidence AS confidence,
-       CAST(socials AS JSON) as socials,    -- Ensure each attribute can be serialized to JSON
-       geometry                             -- DuckDB understands this to be a geometry type
-    FROM read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=places/type=place/*', filename=true, hive_partitioning=1)
-    WHERE categories.primary = 'pizza_restaurant'
+  SELECT
+    id,
+    names.primary as name,
+    confidence AS confidence,
+    CAST(socials AS JSON) as socials,    -- Ensure each attribute can be serialized to JSON
+    geometry                             -- DuckDB understands this to be a geometry type
+  FROM
+    read_parquet('s3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=places/type=place/*', filename=true, hive_partitioning=1)
+  WHERE
+    categories.primary = 'pizza_restaurant'
     AND bbox.xmin BETWEEN -75 AND -73       -- Only use the bbox min values
     AND bbox.ymin BETWEEN 40 AND 41         -- because they are point geometries.
-
 ) TO 'nyc_pizza.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
