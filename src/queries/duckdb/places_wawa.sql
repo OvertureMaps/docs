2c6eb3cffa65d08930569bb1cc5c6e5d2d841192
Dana Bauer <dana.bauer@gmail.com>
2026-03-23 13:01:15 -0400

    [BUG] fix queries (#314)
    * fix queries
    
    Signed-off-by: Dana Bauer <dana.bauer@gmail.com>
    
    * fight the linter
    
    Signed-off-by: Dana Bauer <dana.bauer@gmail.com>
    
    ---------
    
    Signed-off-by: Dana Bauer <dana.bauer@gmail.com>

---
+++ src/queries/duckdb/places_wawa.sql
@@ -0,0 +1,20 @@
+LOAD spatial; --noqa
+LOAD httpfs; --noqa
+
+SET s3_region = 'us-west-2';
+
+COPY(
+  SELECT
+    id,
+    names.primary as name,
+    categories.primary as category,
+    addresses[1].freeform as address,
+    geometry
+  FROM read_parquet(
+    's3://overturemaps-us-west-2/release/__OVERTURE_RELEASE/theme=places/type=place/*',
+    filename=true, hive_partitioning=1)
+  WHERE
+    names.primary ILIKE '%wawa%'
+    AND bbox.xmin BETWEEN -76.5 AND -74.5
+    AND bbox.ymin BETWEEN 39.5 AND 40.5
+) TO 'wawa_stores.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
