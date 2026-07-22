7f91b0b7cea5d083a50c6bf22f0b3891a68bf600
Warren Ehrenfried <warrenehrenfried@gmail.com>
2026-07-22 15:28:59 -0400

    buildings, calendar, and docusaurus
    Signed-off-by: Warren Ehrenfried <warrenehrenfried@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ function getFromEnvironment(variableName, defaultValue) {
 }
 
 function getLatestOvertureRelease() {
-  const fallback = '2026-06-17.0';
+  const fallback = '2026-07-22.0';
   try {
     const { execSync } = require('child_process');
     const response = execSync('curl -s https://stac.overturemaps.org/catalog.json', {
