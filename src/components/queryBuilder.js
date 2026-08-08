2a90977c88a302d410b0931c12e682b8a310ee57
TAKAHASHI Shuuji <id@shuuji3.xyz>
2026-08-08 16:03:35 +0900

    fix(overture-tiles): use QueryBuilder component for dynamic release paths for tile URL
    Signed-off-by: TAKAHASHI Shuuji <id@shuuji3.xyz>

--- src/components/queryBuilder.js
+++ src/components/queryBuilder.js
@@ -1,28 +1,37 @@
 import CodeBlock from '@theme/CodeBlock';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
+function replacePlaceholders(str, release) {
+  return str
+    .replaceAll('__OVERTURE_RELEASE', release)
+    .replaceAll(
+      '__ATHENA_OVERTURE_RELEASE',
+      'v' + release.replaceAll('.', '_').replaceAll('-', '_')
+    )
+    .replaceAll('__PMTILES_OVERTURE_RELEASE', release.split('.', 1));
+}
+
 export default function QueryBuilder(args) {
   const {
     siteConfig: { customFields },
   } = useDocusaurusContext();
 
-  var rendered_query = args.query.replaceAll('__OVERTURE_RELEASE', customFields.overtureRelease);
-
-  rendered_query = rendered_query.replaceAll(
-    '__ATHENA_OVERTURE_RELEASE',
-    'v' + customFields.overtureRelease.replaceAll('.', '_').replaceAll('-', '_')
-  );
-
-  rendered_query = rendered_query.replaceAll(
-    '__PMTILES_OVERTURE_RELEASE',
-    customFields.overtureRelease.split('.', 1)
-  );
-
+  var text = replacePlaceholders(args.query, customFields.overtureRelease);
+  var title = args.title && replacePlaceholders(args.title, customFields.overtureRelease);
   var lang = args.language || 'sql';
 
+  if (args.href) {
+    var href = replacePlaceholders(args.href, customFields.overtureRelease);
+    return <a href={href} title={title}>{text}</a>;
+  }
+
+  if (args.inline) {
+    return <code>{text}</code>;
+  }
+
   return (
-    <CodeBlock language={lang} title={args.title}>
-      {rendered_query}
+    <CodeBlock language={lang} title={title}>
+      {text}
     </CodeBlock>
   );
 }
