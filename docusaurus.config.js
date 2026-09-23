7730bfe32ccf697bee4f1f3b78dbed0c138e6314
Stephen Epps <stephen@overturemaps.org>
2026-09-23 08:57:48 -0700

    update docusaurus confirg
    Signed-off-by: Stephen Epps <stephen@overturemaps.org>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ function getFromEnvironment(variableName, defaultValue) {
 }
 
 function getLatestOvertureRelease() {
-  const fallback = '2026-08-19.0';
+  const fallback = '2026-09-23.0';
   try {
     const { execSync } = require('child_process');
     const response = execSync('curl -s https://stac.overturemaps.org/catalog.json', {
